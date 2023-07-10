c3763ad1b53bcca8a3c2991d09f8760adbf55b3d
Maksim Shvets <mvshvets1@gmail.com>
2023-07-10 21:03:16 +0300

    правки

--- frontend/pages/Notes/Note/index.tsx
+++ frontend/pages/Notes/Note/index.tsx
@@ -3,13 +3,14 @@ import { GameFullContract } from '@/src/api'
 import Block from '@/src/components/Block'
 import s from '@/styles/Note.module.scss'
 import { Space, Typography } from 'antd'
+import CupIcon from '@/public/cup.svg'
 
 interface Props {
-  game: GameFullContract,
+  game: GameFullContract
 }
 
 export default function Note({ game }: Props) {
-    return (
+  return (
     <Block className={s.wrapper}>
       {/*TODO: убрать required*/}
       {[game.firstTeam!, game.secondTeam!].map((team, i) => (
@@ -24,16 +25,16 @@ export default function Note({ game }: Props) {
               {/*TODO: убрать required*/}
               {team.charactersInTeam!.map(hero => (
                 // TODO : убрать required
-                <HeroItem
-                  hero={hero}
-                  key={hero.id}
-                />
+                <HeroItem hero={hero} key={hero.id} />
               ))}
             </Space>
           </div>
+          {!i && <CupIcon />}
         </Space>
       ))}
-      <Typography.Text className={s.comment}>{game.comment}</Typography.Text>
+      <div className={s.comment}>
+        <Typography.Text>{game.comment}</Typography.Text>
+      </div>
     </Block>
   )
 }
